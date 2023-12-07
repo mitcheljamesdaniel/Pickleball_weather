a26a77663259b2dfb5ff796749483bfdb3c152cf
MitchelJDaniel <mitcheljamesdaniel@gmail.com>
2023-12-07 18:48:53 -0500

    Added complete app code, and remove directories for shiny hosted app

---
+++ README.md
@@ -0,0 +1,19 @@
+Pickleball Weather Forecaster
+-----------------------
+
+You're probably sick of hearing it by now: pickleball is the fastest growing sport in the US. If you're part of the growing legion of pickleball players that regularly plays outdoors, you probably want a quick and convenient way to figure out when weather conditions will be suitable for pickleball. This app retrieves the 5-day weather forecast for your location, determines which times are suitable to your personal weather preferences, and presents the information in an easy-to-read weather forecast table.
+
+I built the app UI using Shiny for Python. The code uses openweather APIs to retrieve geolocation and weather data. The app is hosted on GitHub Gist and uses Shinylive to run.
+
+Instructions
+-----------------------
+
+<a href="https://shinylive.io/py/app/#gist=6f1d47448c29bc115028aef014233468"> Try out the app here. </a>
+
+OR
+
+<a href="https://shinylive.io/py/editor/#gist=6f1d47448c29bc115028aef014233468"> Access the editor here</a>, which allows you to fiddle with the underlying code and see the effect of the app.
+
+Type in your city name and select your state (US locations only), then click the 'Generate Calendar' button see the pickleball forecast.
+
+You can customize the temperature range, chance or precipitation, and max wind speed you enjoy playing in. The forecast will automatically update to reflect these changes.
--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 #pickleball weather forecast shiny app
 
-import requests
+from doctest import OutputChecker
 import json
 import shiny
 import pandas
 import time
+import pyodide.http
 
 def get_time(x):
     return time.strptime(x, '%H:%M:%S')
@@ -25,79 +26,91 @@ states = ["AL", "AK", "AZ", "AR", "AS", "CA", "CO", "CT", "DE", "DC", "FL", "GA"
           "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "MP", "OH", "OK", "OR", "PA", 
           "PR", "RI", "SC", "SD", "TN", "TX", "TT", "UT", "VT", "VA", "VI", "WA", "WV", "WI", "WY"]
 
+current_data = shiny.reactive.Value(None)
+
 #create the shiny ui
 app_ui = shiny.ui.page_fluid(
-    shiny.ui.panel_title("Find a time to play outdoor pickleball"),
+    shiny.ui.panel_title("When to play outdoor pickleball"),
     shiny.ui.layout_sidebar( #create two-panel layout
         shiny.ui.panel_sidebar( #place inputs in the sidebar
+            shiny.ui.output_text("instructions_a"),
             shiny.ui.input_text("city", "City:", placeholder="Enter city name"),
             shiny.ui.input_selectize("state", "State:", states),
             shiny.ui.input_action_button("compute", "Generate Calendar", class_="btn-primary"),
+            shiny.ui.output_text("instructions_b"),
             shiny.ui.input_slider("temp_range", "Temperature range (F):", value = (40, 90), min = 30, max = 110),
             shiny.ui.input_slider("max_precip", "Max chance of precipitation (%):", value = 30, min = 0, max = 100),
             shiny.ui.input_numeric("max_speed", "Max wind speed (mph):", 15)
         ),
         shiny.ui.panel_main( #place outputs in the main panel
-            shiny.ui.output_text("geo_API_test"),
             shiny.ui.output_text("weather_test"),
-            shiny.ui.output_table("make_table"),
-            shiny.ui.output_text("make_legend")
+            shiny.ui.output_text("make_legend"),
+            shiny.ui.output_table("make_table")
             )
     )
 )
 
 #create the server function
 def server(input, output, session):
-    @shiny.reactive.Calc
-    def geo_params(): #return a dictionary with parameters for the geo API request, including user-specified city and state
-        return {
-            "q": input.city() + "," + input.state() +  ",US",
-            "appid": "b0fb1574b0176f4025a21cfea6f99005",
-            "limit": 1
-            }
-    
-    @shiny.reactive.Calc
-    def geo_request(): #return geo location data from API request
-        input.compute() #reexecute this function in response to input button press
-        with shiny.reactive.isolate(): #do NOT reexecute this function in response to changes in its dependencies (city or state inputs) to avoid excess API requests
-            return requests.get("http://api.openweathermap.org/geo/1.0/direct?", params = geo_params())
+    @output
+    @shiny.render.text
+    def instructions_a():
+        return "Find times in the 5-day weather forecast that are suitable for outdoor Pickleball! Type in your city and select your state (US locations only). Then, click 'Generate Calendar' to see when conditions will be suitable in your location."
     
     @output
     @shiny.render.text
-    def geo_API_test(): #print error message if geo API request was unsuccessful (usually happens because city is misspelled or city and state are mismatched)
-        if geo_request().status_code != 200 or len(geo_request().json()) != 1:
-            return  "Error: Please check that the location information is accurate."
-    
+    def instructions_b():
+        return "You can customize the range of weather conditions that are considered suitable to fit your preferences. Drag the sliders to temperature range and max chance of precipitation. Type or use the scroller to set the max wind speed. The calendar will automatically update to reflect these changes."
+
     @shiny.reactive.Calc
-    def weather_params(): #create dictionary parameters for weather API request, including lat and lon from geo data
-        return {
-            "lat": geo_request().json()[0]['lat'],
-            "lon": geo_request().json()[0]['lon'],
-            "appid": "b0fb1574b0176f4025a21cfea6f99005",
-            "exclude": "current,minutely,hourly",
-            "units": "imperial"
-            }
-    
-    @shiny.reactive.Calc
-    def weather_request(): #return weather data for the chosen location
+    async def geo_request(): #return geo location data from API request
+        API = "b0fb1574b0176f4025a21cfea6f99005"
         input.compute() #reexecute this function in response to input button press
         with shiny.reactive.isolate(): #do NOT reexecute this function in response to changes in its dependencies (city or state inputs) to avoid excess API requests
-            return requests.get("http://api.openweathermap.org/data/2.5/forecast?id=524901", params = weather_params())
+            geo_response = await pyodide.http.pyfetch(f"https://api.openweathermap.org/geo/1.0/direct?q={input.city()},{input.state()},US&limit=1&appid={API}")
+            geo_data = await geo_response.json()
+            if geo_response.status != 200 or len(geo_data) != 1: #return string error message if geo API request was unsuccessful (usually happens because city is misspelled or city and state are mismatched)
+                output_string.set("Error: Please check that the location information is accurate. Bad geolocation request. Check your city name spelling.")
+                return None
+            geo_data = geo_data[0]
+            return [geo_data['lat'], geo_data['lon']]
+
+    @output
+    @shiny.render.text
+    async def geo_test(): #if the request failed (and returned a string), print an error message
+        geo_data = await geo_request()
+        if geo_data == None:
+            return "Error: Please check that the location information is accurate."
+        return ""
+    
+    @shiny.reactive.Effect
+    @shiny.reactive.event(input.compute)
+    async def weather_request(): #return weather data for the chosen location
+        weather_ID = "b0fb1574b0176f4025a21cfea6f99005"
+        geo_data = await geo_request()
+        if geo_data == None:
+            return
+        weather_response = await pyodide.http.pyfetch(f"https://api.openweathermap.org/data/2.5/forecast?id=524901&units=imperial&lat={geo_data[0]}&lon={geo_data[1]}&exclude=current,minutely,hourly&appid={weather_ID}")
+        weather_data = await weather_response.json()
+        if weather_response.status != 200 or len(weather_data) == 0:
+            output_string.set("Weather API request failed. Try again.")
+            return
+        output_string.set(f"{input.city()}, {input.state()}")
+        process_the_weather_data(weather_data)
+    
+    def process_the_weather_data(data):
+        weather_list = [weather(i) for i in data['list']]
+        filtered_list = [day for day in weather_list if day.check_day()]
+        
+        list_of_days = day_sorting(filtered_list)
+        current_data.set(list_of_days)
+
+    output_string = shiny.reactive.Value("")
     
     @output
     @shiny.render.text
-    def weather_test(): #print error message if weather API request was unsuccessful
-        if weather_request().status_code != 200 or len(weather_request().json()) == 0:
-            return "Error: weather data is not currently available for this location. Please try again later."
-    
-    def weather_list(): #create list of weather objects, one for each entry in the weather dictionary
-        return [weather(i) for i in weather_request().json()['list']]
-    
-    def day_index(): #create list of booleans for whether the weather object at each position is during the day (6:00 am to 12:00 pm)
-        return [j.check_day() for j in weather_list()]
-    
-    def filtered_list(): #filter the list of weather objects to include only those for daytime entries
-        return [weather_list()[k] for k in range(len(weather_list())) if day_index()[k]]
+    def weather_test(): #print error message if weather API request returned a string (i.e. failed)
+        return output_string()
     
     @shiny.reactive.Calc
     def min_temp(): #get the user-specified minimum temperature
@@ -115,20 +128,19 @@ def server(input, output, session):
     def max_precip(): #get the user-specified maximum precipitation
         return input.max_precip()
     
-    @shiny.reactive.Calc
-    def day_sorting(): #create a list of lists, where each of the sublists contains the weather objects for a given day
+    def day_sorting(filtered_list): #create a list of lists, where each of the sublists contains the weather objects for a given day
         list_of_days = []
         list_of_days.append([])
-        prev_date = filtered_list()[0].date
+        prev_date = filtered_list[0].date
         day_index = 0
-        for m in range(len(filtered_list())):
-            if filtered_list()[m].date == prev_date: #place all weather objects with the same data in the same sublist
-                list_of_days[day_index].append(filtered_list()[m])
+        for m in range(len(filtered_list)):
+            if filtered_list[m].date == prev_date: #place all weather objects with the same data in the same sublist
+                list_of_days[day_index].append(filtered_list[m])
             else: #each time the weather object has a different date than the weather object before it, place it in a new sublist
                 list_of_days.append([])
                 day_index = day_index + 1
-                list_of_days[day_index].append(filtered_list()[m])
-                prev_date = filtered_list()[m].date
+                list_of_days[day_index].append(filtered_list[m])
+                prev_date = filtered_list[m].date
         return list_of_days
     
     def time_windows(): #create list of time windows, to be used as the row names in the output table
@@ -137,7 +149,9 @@ def server(input, output, session):
     def day_dictionary(): #return a dictionary where each key is a column heading for the output table, and each value is the corresponding list of entires (time windows or weather objects)
         day_dictionary = {}
         day_dictionary["Time of day"] = time_windows() #create the dictionary entry for the row names
-        for day in day_sorting(): #for each day, create a list of weather reports describing whether the conditions are suitable for pickleball
+        if current_data() == None:
+            return day_dictionary
+        for day in current_data(): #for each day, create a list of weather reports describing whether the conditions are suitable for pickleball
             day_report = []
             for time_point in ("06:00:00", "09:00:00", "12:00:00", "15:00:00", "18:00:00"): #create a blank report for each time window on the first day that preceeds the first weather entry
                 if get_time(day[0].time) > get_time(time_point):
@@ -148,13 +162,13 @@ def server(input, output, session):
                 else: #if conditions are not suitable, create a report stating why
                     bad_report = ""
                     if o.temp < min_temp():
-                        bad_report = bad_report + f"\U0001F976"
+                        bad_report = bad_report + f"\U0001F976 " + str(o.temp) + u'\N{DEGREE SIGN}' + "F<br>"
                     if o.temp > max_temp():
-                        bad_report = bad_report + f"\U0001F975"
+                        bad_report = bad_report + f"\U0001F975 " + str(o.temp) + u'\N{DEGREE SIGN}' + "F<br>"
                     if o.speed > max_speed():
-                        bad_report = bad_report + f"\U0001f32a\uFE0F"
+                        bad_report = bad_report + f"\U0001f32a\uFE0F " + str(o.speed) + " mph<br>"
                     if o.precip > max_precip():
-                        bad_report = bad_report + f"\U0001f327\uFE0F"
+                        bad_report = bad_report + f"\U0001f327\uFE0F " + str(o.precip) + "%"
                     day_report.append(bad_report)
             for time_point in ("21:00:00", "18:00:00", "15:00:00", "12:00:00", "09:00:00"): #for every time window on the last day that's after the last weather entry, create a blank report
                 if get_time(day[-1].time) < get_time(time_point):
@@ -173,13 +187,9 @@ def server(input, output, session):
     @output
     @shiny.render.text
     def make_legend(): #create a legend explaining what each emoji means
-        if geo_request().status_code != 200 or len(geo_request().json()) != 1:
+        if current_data() == None: #don't make legend unless there is data in the table
             return ""
         else:
             return "\U0001f600 = good conditions; \U0001F976 = too cold; \U0001F975 = too hot; \U0001f32a\uFE0F = too windy; \U0001f327\uFE0F = high chance of precipitation"
 
 app = shiny.App(ui=app_ui, server=server) 
-
-#Indicate in readme somewhere that it's (currently) limited to cities in the US?
-
-#change the column titles to days of the week? - might need to access a calender to do this
